1f47106efc1240a5ba9a847bec545945ce09e53a
shuaigehan <zh325700@gmail.com>
2018-08-16 17:52:08 +0800

    first commit with first tutorial

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y,fruitX,fruitY,score;
+enum eDirection {STOP = 0, LEFT,RIGHT,UP,DOWN};
+eDirection dir;
+
+void Setup(){
+	gameOver = false;
+	dir = STOP;
+	x = width / 2;
+	y = height / 2;
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+	score = 0;
+}
+
+void Draw() {
+	system("cls"); // system("clear"); if you are in linux
+	for (int i = 0;i<width;i++) {
+		cout << "#";
+	}
+	cout << endl;
+	for (int i = 0; i<height; i++) {
+		for (int j = 0;j<width;j++) {
+			if (j==0 || j==width-1) {
+				cout << "#";
+			}
+			else {
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}
+	for (int i = 0; i<width; i++) {
+		cout << "#";
+	}
+	cout << endl;
+}
+
+void Input() {
+}
+
+void Logic() {
+
+}
+
+int main(){
+	Setup();
+	while (!gameOver) {
+		Draw();
+		Input();
+		Logic();
+		//Sleep(10);
+	}
+	system("pause");
+	return 0;
+}
